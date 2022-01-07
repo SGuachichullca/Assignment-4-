2a9325b3bf75e7f60dbdc005fd14126f023ea56d
Sebastian Guachichullca <sebastian.guachichullca@cix.csi.cuny.edu>
2022-01-07 08:59:25 -0500

    Assignment 4

---
+++ index.js
@@ -0,0 +1,35 @@
+
+//Q1
+
+function q1Argue () {
+    document.querySelector(".q1 p").innerHTML = "    I'm right!"
+}
+
+function q1ArgueBack () {
+    document.querySelector(".q1 p").innerHTML = "   No, I'm right!";
+}
+//////////////////////////////////////////////////////////////
+
+//Q2
+
+const hoverElement = document.querySelector(".q2 h2");
+
+hoverElement.addEventListener("mouseover", function (event) {
+    alert("Hey! I told you not hover over me!");
+
+})
+//////////////////////////////////////////////////////////////
+
+//Q4
+
+const password = document.getElementById("password");
+const heading = document.querySelector(".prompt");
+    
+    function q4Submit() {
+       
+        if (password.value == "password123") {
+            heading.innerHTML = "Welcome " + document.querySelector("#username").value + ".";
+        } else {
+            alert ("The password you have entered is invalid. Please try again.")
+        }
+    }
